94e4d062d91c5193f063dcaad726b3217c161773
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-05-16 01:48:33 +0200

    [tx-robot] updated from transifex

--- l10n/pt_BR.js
+++ l10n/pt_BR.js
@@ -15,6 +15,7 @@ OC.L10N.register(
     "Couldn't send reset email. Please contact your administrator." : "Não foi possível enviar e-mail de redefinição. Por favor, contate o administrador.",
     "Error while sharing" : "Erro ao compartilhar",
     "Error" : "Erro",
+    "Share with {myname}" : "Compartilhado com {myname}",
     "Add {unknown} (guest)" : "Adicionar {unknown} (convidado)",
     "No users or groups found for {search}" : "Nenhum usuário ou grupo encontrado para {search}",
     "No users found for {search}" : "Nenhum usuário encontrado para {search}",
