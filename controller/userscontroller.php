186226677b03cd28525aca0653e4473da60760cd
Thomas Müller <thomas.mueller@tmit.eu>
2017-09-23 16:27:21 +0200

    Add event driven password generation

--- controller/userscontroller.php
+++ controller/userscontroller.php
@@ -23,23 +23,20 @@
 namespace OCA\Guests\Controller;
 
 
-use OCA\Guests\Mail;
 use OC\AppFramework\Http;
 use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http\DataResponse;
 use OCP\IConfig;
-use OCP\IGroupManager;
 use OCP\IL10N;
 use OCP\IRequest;
 use OCP\IUserManager;
 use OCP\Mail\IMailer;
 use OCP\Security\ISecureRandom;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\EventDispatcher\GenericEvent;
 
 class UsersController extends Controller {
-	/**
-	 * @var IRequest
-	 */
-	protected $request;
+
 	/**
 	 * @var IUserManager
 	 */
@@ -57,14 +54,6 @@ class UsersController extends Controller {
 	 */
 	private $mailer;
 	/**
-	 * @var Mail
-	 */
-	private $mail;
-	/**
-	 * @var IGroupManager
-	 */
-	private $groupManager;
-	/**
 	 * @var ISecureRandom
 	 */
 	private $secureRandom;
@@ -76,33 +65,29 @@ class UsersController extends Controller {
 	 * @param string $appName
 	 * @param IRequest $request
 	 * @param IUserManager $userManager
-	 * @param IGroupManager $groupManager
 	 * @param IL10N $l10n
 	 * @param IConfig $config
 	 * @param IMailer $mailer
-	 * @param Mail $mail
 	 * @param ISecureRandom $secureRandom
+	 * @param EventDispatcher $eventDispatcher
 	 */
 	public function __construct($appName,
 								IRequest $request,
 								IUserManager $userManager,
-								IGroupManager $groupManager,
 								IL10N $l10n,
 								IConfig $config,
 								IMailer $mailer,
-								Mail $mail,
-								ISecureRandom $secureRandom
+								ISecureRandom $secureRandom,
+								EventDispatcher $eventDispatcher
 	) {
 		parent::__construct($appName, $request);
 
-		$this->request = $request;
 		$this->userManager = $userManager;
 		$this->l10n = $l10n;
 		$this->config = $config;
 		$this->mailer = $mailer;
-		$this->mail = $mail;
-		$this->groupManager = $groupManager;
 		$this->secureRandom = $secureRandom;
+		$this->eventDispatcher = $eventDispatcher;
 	}
 
 	/**
@@ -141,10 +126,17 @@ class UsersController extends Controller {
 			);
 		}
 
+		$event = new GenericEvent();
+		$this->eventDispatcher->dispatch('OCP\User::createPassword', $event);
+		if ($event->hasArgument('password')) {
+			$password = $event->getArgument('password');
+		} else {
+			$password = $this->secureRandom->generate(20);
+		}
 
 		$user = $this->userManager->createUser(
 			$username,
-			$this->secureRandom->generate(20)
+			$password
 		);
 
 		$user->setEMailAddress($email);
