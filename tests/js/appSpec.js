9bb51977a749754ce3663defff176a55ed83a8b8
Vincent Petry <PVince81@owncloud.com>
2018-01-17 17:33:00 +0100

    Add karma for JS tests

---
+++ tests/js/appSpec.js
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2018 Vincent Petry <pvince81@owncloud.com>
+ *
+ * This file is licensed under the Affero General Public License version 3
+ * or later.
+ *
+ * See the COPYING-README file.
+ *
+ */
+
+describe('App test', function() {
+
+	it('needs some unit tests...', function() {
+		expect(true).toEqual(true);
+	});
+});
