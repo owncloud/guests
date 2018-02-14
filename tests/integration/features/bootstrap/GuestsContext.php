ba89bff8a06f67147811c1eb84389e2822adb136
Phil Davis <phil@jankaritech.com>
2018-02-14 11:36:29 +0545

    Refactor wording of guests feature steps

--- tests/integration/features/bootstrap/GuestsContext.php
+++ tests/integration/features/bootstrap/GuestsContext.php
@@ -42,12 +42,13 @@ class GuestsContext implements Context, SnippetAcceptingContext {
 	}
 
 	/**
-	 * @Given user :user creates guest user :guestDisplayName with email :guestEmail
+	 * @When user :user creates guest user :guestDisplayName with email :guestEmail using the API
+	 * @Given user :user has created guest user :guestDisplayName with email :guestEmail
 	 * @param string $user
 	 * @param string $guestDisplayName
 	 * @param string $guestEmail
 	 */
-	public function userCreatedAGuestUser($user, $guestDisplayName, $guestEmail) {
+	public function userCreatesAGuestUser($user, $guestDisplayName, $guestEmail) {
 		$fullUrl = substr($this->baseUrl, 0, -4) . '/index.php/apps/guests/users';
 		//Replicating frontend behaviour
 		$userName = $this->prepareUserNameAsFrontend($guestDisplayName, $guestEmail);
@@ -72,7 +73,7 @@ class GuestsContext implements Context, SnippetAcceptingContext {
 	}
 
 	/**
-	 * @Then check that user :user is a guest
+	 * @Then user :user should be a guest user
 	 * @param string $guestDisplayName
 	 */
 	public function checkGuestUser($guestDisplayName) {
@@ -81,7 +82,7 @@ class GuestsContext implements Context, SnippetAcceptingContext {
 	}
 
 	/**
-	 * @Then guest user :user is deleted
+	 * @Given guest user :user has been deleted
 	 * @param string $guestDisplayName
 	 */
 	public function deleteGuestUser($guestDisplayName) {
@@ -106,7 +107,8 @@ class GuestsContext implements Context, SnippetAcceptingContext {
 	}
 
 	/**
-	 * @Given guest user :user registers
+	 * @When guest user :user registers
+	 * @Given guest user :user has registered
 	 * @param string $guestDisplayName
 	 */
 	public function guestUserRegisters($guestDisplayName) {
