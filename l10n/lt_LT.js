1b27892f45680973c52bbe2552b34f4b53ec49d6
ownCloud Jenkins <jenkins@owncloud.com>
2018-05-17 00:46:19 +0000

    [tx-robot] updated from transifex

--- l10n/lt_LT.js
+++ l10n/lt_LT.js
@@ -32,4 +32,4 @@ OC.L10N.register(
     "Limit guest access to an app whitelist" : "Riboti svečio priėjimą applikacijos sarašuose",
     "Reset whitelist" : "Nustatytį iš naujo baltą sąrašą"
 },
-"nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && (n%100<10 || n%100>=20) ? 1 : 2);");
+"nplurals=4; plural=(n % 10 == 1 && (n % 100 > 19 || n % 100 < 11) ? 1 : (n%10 >= 2 && n % 10 <=9) && (n % 100 > 19 || n % 100 < 11) ? 3 : n % 1 != 0 ? 4);");
