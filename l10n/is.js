77d608ea00d92e2a4f070bd3f945f8e7c83c8231
ownClouders <devops@owncloud.com>
2019-07-02 05:09:59 +0000

    [tx] updated from transifex

--- l10n/is.js
+++ l10n/is.js
@@ -6,6 +6,7 @@ OC.L10N.register(
     "Reset" : "Endurstilla",
     "Error" : "Villa",
     "Error while sharing" : "Villa við deilingu",
+    "Add {unknown}" : "Bæta við {unknown}",
     "Error loading activities" : "Villa við hleðslu virkniupplýsinga",
     "Access to this resource is forbidden for guests." : "Gestum er bannaður aðgangur að þessu tilfangi.",
     "Invalid mail address" : "Ógilt tölvupóstfang",
