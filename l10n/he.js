4c5af435bc5012bf9bb661f292601dfd0de6a3b4
ownClouders <devops@owncloud.com>
2024-03-05 00:38:30 +0000

    [tx] updated from transifex

--- l10n/he.js
+++ l10n/he.js
@@ -33,4 +33,4 @@ OC.L10N.register(
     "Limit guest access to an app whitelist" : "מגביל גישת משתמשים ברמת אורח לרשימת יישום לבנה",
     "Reset whitelist" : "מאפס רשימה לבנה"
 },
-"nplurals=4; plural=(n == 1 && n % 1 == 0) ? 0 : (n == 2 && n % 1 == 0) ? 1: (n % 10 == 0 && n % 1 == 0 && n > 10) ? 2 : 3;");
+"nplurals=3; plural=(n == 1 && n % 1 == 0) ? 0 : (n == 2 && n % 1 == 0) ? 1: 2;");
